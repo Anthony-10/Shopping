214edbdb6c41358d55a5b2571cddfe4951c2a234
anthony <Muthumatonito681@gmail.com>
2022-02-05 04:03:30 +0300

    1. Updated userCartInfo.dart,

--- lib/buy/cart/widget/userCartInfo.dart
+++ lib/buy/cart/widget/userCartInfo.dart
@@ -44,109 +44,113 @@ class _UserCartInfoState extends State<UserCartInfo> {
                     physics: BouncingScrollPhysics(),
                     itemCount: snapshot.data.size,
                     itemBuilder: (context, index) {
-                      return Card(
-                        child: Padding(
-                          padding: const EdgeInsets.all(9.0),
-                          child: Column(
-                            children: [
-                              Row(
-                                children: [
-                                  Container(
-                                    decoration: BoxDecoration(
-                                      borderRadius: BorderRadius.circular(20),
-                                    ),
-                                    height: Get.height * .15,
-                                    width: Get.width * .25,
-                                    child: Card(
-                                      child: CachedNetworkImage(
-                                        cacheManager:
-                                            buyController.customCacheManager,
-                                        imageUrl: snapshot
-                                            .data.docs[index]['image']
-                                            .toString(),
-                                        fit: BoxFit.fill,
-                                        placeholder: (context, url) =>
-                                            Container(
-                                          color: Colors.black12,
+                      return FittedBox(
+                        child: Card(
+                          child: Padding(
+                            padding: const EdgeInsets.all(9.0),
+                            child: Column(
+                              children: [
+                                Row(
+                                  children: [
+                                    Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(20),
+                                      ),
+                                      height: Get.height * .15,
+                                      width: Get.width * .25,
+                                      child: Card(
+                                        child: CachedNetworkImage(
+                                          cacheManager:
+                                              buyController.customCacheManager,
+                                          imageUrl: snapshot
+                                              .data.docs[index]['image']
+                                              .toString(),
+                                          fit: BoxFit.fill,
+                                          placeholder: (context, url) =>
+                                              Container(
+                                            color: Colors.black12,
+                                          ),
+                                          errorWidget: (context, url, error) =>
+                                              Container(
+                                            color: Colors.black12,
+                                            child: Icon(Icons.error,
+                                                color: Colors.red),
+                                          ),
                                         ),
-                                        errorWidget: (context, url, error) =>
-                                            Container(
-                                          color: Colors.black12,
-                                          child: Icon(Icons.error,
-                                              color: Colors.red),
+                                        semanticContainer: true,
+                                        clipBehavior:
+                                            Clip.antiAliasWithSaveLayer,
+                                        elevation: 20.0,
+                                        color: Colors.white,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(10.0),
                                         ),
                                       ),
-                                      semanticContainer: true,
-                                      clipBehavior: Clip.antiAliasWithSaveLayer,
-                                      elevation: 20.0,
-                                      color: Colors.white,
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius:
-                                            BorderRadius.circular(10.0),
+                                    ),
+                                    SizedBox(
+                                      width: Get.width * .15,
+                                    ),
+                                    Container(
+                                      width: Get.width * .2,
+                                      child: Column(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.start,
+                                        children: [
+                                          Text(
+                                            'name: ${snapshot.data.docs[index]['name'].toString()}',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 17),
+                                          ),
+                                          SizedBox(
+                                            height: Get.height * .02,
+                                          ),
+                                          Text(
+                                              'price: ${snapshot.data.docs[index]['price'].toString()}'),
+                                          SizedBox(
+                                            height: Get.height * .02,
+                                          ),
+                                          Text(
+                                              'amount: ${snapshot.data.docs[index]['amount'].toString()}'),
+                                        ],
                                       ),
                                     ),
-                                  ),
-                                  SizedBox(
-                                    width: Get.width * .15,
-                                  ),
-                                  Container(
-                                    width: Get.width * .2,
-                                    child: Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Text(
-                                          'name: ${snapshot.data.docs[index]['name'].toString()}',
-                                          style: TextStyle(
-                                              fontWeight: FontWeight.bold,
-                                              fontSize: 17),
-                                        ),
-                                        SizedBox(
-                                          height: Get.height * .02,
-                                        ),
-                                        Text(
-                                            'price: ${snapshot.data.docs[index]['price'].toString()}'),
-                                        SizedBox(
-                                          height: Get.height * .02,
-                                        ),
-                                        Text(
-                                            'amount: ${snapshot.data.docs[index]['amount'].toString()}'),
-                                      ],
+                                    SizedBox(
+                                      width: Get.width * .12,
                                     ),
-                                  ),
-                                  SizedBox(
-                                    width: Get.width * .12,
-                                  ),
-                                  IconButton(
-                                      onPressed: () {
-                                        cartController.sllerId = snapshot
-                                            .data.docs[index].reference
-                                            .delete();
-                                        //TODO
-                                        //Adding the numbers even
-                                        // when removing the items
-                                        selectedPrice = snapshot
-                                            .data.docs[index]['price']
-                                            .toString();
-                                        selectedAmount = snapshot
-                                            .data.docs[index]['amount']
-                                            .toString();
-                                        int sPrice = int.parse(selectedPrice);
-                                        int sAmount = int.parse(selectedAmount);
-                                        cartController.sumAmountPrice =
-                                            sPrice * sAmount;
-                                        print(
-                                            '${cartController.sumAmountPrice},lllllllllllllllll');
-                                        cartController.subFunction();
-                                      },
-                                      icon: Icon(Icons.close)),
-                                ],
-                              ),
-                            ],
+                                    IconButton(
+                                        onPressed: () {
+                                          cartController.sllerId = snapshot
+                                              .data.docs[index].reference
+                                              .delete();
+                                          //TODO
+                                          //Adding the numbers even
+                                          // when removing the items
+                                          selectedPrice = snapshot
+                                              .data.docs[index]['price']
+                                              .toString();
+                                          selectedAmount = snapshot
+                                              .data.docs[index]['amount']
+                                              .toString();
+                                          int sPrice = int.parse(selectedPrice);
+                                          int sAmount =
+                                              int.parse(selectedAmount);
+                                          cartController.sumAmountPrice =
+                                              sPrice * sAmount;
+                                          print(
+                                              '${cartController.sumAmountPrice},lllllllllllllllll');
+                                          cartController.subFunction();
+                                        },
+                                        icon: Icon(Icons.close)),
+                                  ],
+                                ),
+                              ],
+                            ),
                           ),
-                        ),
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
                         ),
                       );
                     });
